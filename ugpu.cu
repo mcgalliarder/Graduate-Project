f66cf199af88d5c92b7251ff80fb7f613ecb3653
sambarr12 <39743215+sambarr12@users.noreply.github.com>
2018-06-29 19:00:03 -0400

    Delete ugpu.cu

--- ugpu.cu
+++
@@ -1,7 +0,0 @@
-void unroll_gpu(int C, int H, int W, int K, float* X, float* X_unroll) {
- int H_out = H - K + 1;
- int W_out = W - K + 1;
-int num_threads = C * H_out * W_out;
-int num_blocks = ceil((C * H_out * W_out) / CUDA MAX_NUM_THREADS);
-unroll_kernel<<<num_blocks,CUDA MAX_NUM_THREADS>>>();
-}
